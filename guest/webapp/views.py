33c95a0c584e4242463475b9af2a835d62ee42dc
apple </Kvadrat2020/>
2022-07-02 08:13:13 -0400

    updated create.html and index

--- guest/webapp/views.py
+++ guest/webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from webapp.models import Book
+from webapp.models import Book, STATUS_CHOICES
 
 
 def index_view(request):
@@ -12,13 +12,13 @@ def index_view(request):
 
 def create_book(request):
     if request.method == "GET":
-        return render(request, "create.html")
+        return render(request, "create.html", {"statuses": STATUS_CHOICES})
     else:
         guest_name = request.POST.get("guest_name")
         from_email = request.POST.get("from_email")
         content = request.POST.get("content")
-        new_book = Book.objects.create(guest_name=guest_name, from_email=from_email, content=content)
+        status = request.POST.get("status")
+        new_book = Book.objects.create(guest_name=guest_name, from_email=from_email, content=content, status=status)
         context = {"book": new_book}
 
         return render(request, 'book_view.html', context)
-
